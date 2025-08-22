78e03783aa4285dc32cde126742586ba6c2ee14b
Prasanna <prasannhanchinamani.15@gmail.com>
2025-08-22 15:43:38 +0530

    Remove All the Occuarance

---
+++ dsa/collections_list/RemoveAllOcuuarnce.java
@@ -0,0 +1,47 @@
+package dsa.collections_list;
+
+import dsa.linkedlist.Node;
+
+public class RemoveAllOcuuarnce {
+    public static Node removeAlloccurance(Node head, int value) {
+        while (head != null && head.val.equals(value)) {
+            head = head.next;
+        }
+        Node current = head;
+        Node prev = null;
+        while (current != null) {
+            if (current.val.equals(value)) {
+                prev.next = current.next;
+                current = current.next;
+            } else {
+                prev = current;
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    public static void displayList(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("END");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(2);
+        head.next = new Node(2);
+        head.next.next = new Node(1);
+        head.next.next.next = new Node(8);
+        head.next.next.next.next = new Node(2);
+
+        System.out.println("Original list:");
+        displayList(head);
+        head = removeAlloccurance(head, 2);
+
+        System.out.println("List after removing all occurrences of 2:");
+        displayList(head);
+    }
+}
