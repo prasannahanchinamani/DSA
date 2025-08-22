23d1fac9c4ef6ec338f6369978bda9f71ecf7417
Prasanna <prasannhanchinamani.15@gmail.com>
2025-08-22 15:48:36 +0530

    Remove All occurance in both built in Linkedlist and Dynamic linkedlist

--- dsa/collections_list/RemoveAllOcuuarnce.java
+++ dsa/collections_list/RemoveAllOcuuarnce.java
@@ -2,6 +2,8 @@ package dsa.collections_list;
 
 import dsa.linkedlist.Node;
 
+import java.util.LinkedList;
+
 public class RemoveAllOcuuarnce {
     public static Node removeAlloccurance(Node head, int value) {
         while (head != null && head.val.equals(value)) {
@@ -30,6 +32,10 @@ public class RemoveAllOcuuarnce {
         System.out.println("END");
     }
 
+    public static void removeoccuancebuiltin(LinkedList<Integer> list, int value) {
+        list.removeIf(n -> n.equals(value));
+    }
+
     public static void main(String[] args) {
         Node head = new Node(2);
         head.next = new Node(2);
@@ -40,8 +46,20 @@ public class RemoveAllOcuuarnce {
         System.out.println("Original list:");
         displayList(head);
         head = removeAlloccurance(head, 2);
-
+        System.out.println();
         System.out.println("List after removing all occurrences of 2:");
         displayList(head);
+        //
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(2);
+        list.add(2);
+        list.add(1);
+        list.add(8);
+        list.add(2);
+
+        System.out.println("Original list: " + list);
+        int valueToRemove = 2;
+        removeoccuancebuiltin(list,valueToRemove);
+        System.out.println("List after removing all occurrences of " + valueToRemove + ": " + list);
     }
 }
